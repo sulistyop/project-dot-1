360163e77166c7f6933e842dfc1a1565435cb319
Sulis <sulistyopradana59@gmail.com>
2023-02-16 18:40:05 +0700

    styling dengan ant design

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import 'antd/dist/reset.css'
+import { Button, Card, Space,Avatar,Spin } from 'antd'
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+const { Meta } = Card;
+
+const Timer = () =>{
+  const [count, setCount] = useState(0);
+  const [start, setStart] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+
+
+  const handleStartStop = () => {
+    setLoading(true);
+    setStart(start => !start)
+    if(start){
+      setLoading(loading => loading = false);
+    }
+  }
+
+  const handleReset = () =>{
+    setCount(0)
+  }
+  
+  useEffect(()=>{
+    const timer = setInterval(()=>{
+      if(start){
+        setCount(count => count + 1)
+      }
+    },1000)
+
+    return () =>{
+      clearInterval(timer)
+    }
+
+    }
+  ,[start])
+
+ 
+
+  return(
+    <div>
+      <Card style={{width: 300,}} cover={
+      <h1>Timer</h1>
+    }
+    actions={[
+      <Button style={{ background: "lime", borderColor: "yellow" }} className={'lime-7'} onClick={handleStartStop}  >{start ? 'Stop' : 'Start'}</Button>
+      ,
+      <div style={{ fontSize:"16pt"}}>{count}</div>,
+      <Button style={{ background: "red", borderColor: "yellow", color: "white" }} onClick={handleReset} disabled={start}>Reset</Button>,
+    ]}
+  >
+     <Spin spinning={loading}>
+   
+      </Spin>
+  
+  </Card>
+    </div>
+  )
+}
+
+
 
 function App() {
   const [count, setCount] = useState(0)
+  
+
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <Timer/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
+    
   )
 }
 
